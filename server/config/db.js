e3c2d8b253404d74045ba73242ff958166f30dc0
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-11-23 21:11:25 -0800

    created server & db connection

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.ak6deqi.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("DB Connection Successfull");
+})
+.catch(err=>{
+    console.log("Error In DB Connection", err);
+})
--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
 
-console.log('Server-side code running');
+
+
+app.get('/',(req, res)=>{
+    res.send("Welcome To Story App Server")
+})
+
+
+app.listen(6001, (err)=>{
+    if(err){
+        console.log("Error In Server", err);
+    }
+    else{
+        console.log("Server Is running");
+    }
+})
